4b0e47f57687a63c32aa5b91bf16f72f9aee85f7
Willian Alves Freitas Foca <willian.freitas@zohomail.com>
2025-09-23 14:54:00 -0300

    Configurações do layout genérico

--- view_configs/shell.sql
+++ view_configs/shell.sql
@@ -1,12 +1,28 @@
 
 /*
-    ARQUIVO RESPONSÁVEL POR RENDERIZAR LAYOUT GENÉRICO DA PÁGINA
+    ROTA RESPONSÁVEL POR RENDERIZAR LAYOUT GENÉRICO DA PÁGINA
 */
 
-SELECT
+-- DEFINE AS VARIÁVEIS UTILIZADAS NA ROTA
+SET tema = (
+    sqlpage.cookie('TEMA')
+);
+SET json_mudar_tema = (
+    '{
+        "title": "Inverter tema",
+        "icon": "'||CASE WHEN $tema = 'light' THEN 'moon' ELSE 'sun' END||'",
+        "link": "\\c\\inverter_tema"
+    }'
+);
+
+SELECT -- COMPONENTE RESPONSÁVEL PELO ESTILO GENÉRICO DA ROTA (A ROTA LOGIN EM ESPECIAL POSSUI UM SHELL PRÓPRIO, TODAS AS OUTRAS ROTAS UTILIZAM O SHELL EM VIEW_CONFIGS)
 'shell' AS component,
-sqlpage.cookie('TEMA') AS theme,
-'\c\css\custom_style.css' AS css,
-'' AS footer,
+'AuditaMarlimAzul' AS title,
+'' AS navbar_title,
+$tema AS theme,
+'' as footer,
 'fluid' AS layout,
-'Teste' AS title;
+'\c\css\custom_style.css' AS css,
+'\c\imagens\logo_marlim.png' AS image,
+'\c\imagens\logo_marlim_favicon.png' AS favicon,
+JSON($json_mudar_tema) AS menu_item;
